881be3298197453595bc84781cad87de21bcd089
shubham1810 <skd.1810@gmail.com>
2017-01-27 17:22:38 +0530

    Register to admin

--- class_03/chatbot01/bot01/models.py
+++ class_03/chatbot01/bot01/models.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class MyData(models.Model):
+	name = models.CharField(max_length=100)
+	institution = models.CharField(max_length=200)
+
+	roll_no = models.IntegerField(null=True)
--- class_03/chatbot01/notes.md
+++ class_03/chatbot01/notes.md
@@ -3,3 +3,6 @@ sudo pip install requests bs4
 {{ variable }}
 
 {% statement %}
+
+code url:
+https://github.com/shubham1810/chatbot_bootcamp
